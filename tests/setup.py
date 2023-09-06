0f4aac6cc03273d3eea8c1782b64d4cad586aa34
Valk Castellani <valkcastellani@gmail.com>
2023-09-06 20:09:57 -0300

    Project Start

---
+++ tests/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='klav-forecaster',
+    version='1.0.0',
+    description='Time Series Forecaster',
+    author='Valk Castellani',
+    author_email='valkcastellani@gmail.com',
+    url='https://github.com/valkcastellani/klav-forecaster',
+)
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.10',
+]
